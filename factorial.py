b2f2803fe009514d3b08f31768432a5cf0e96ec6
muskan <muskan22110@gmail.com>
2026-06-09 22:02:19 +0530

    Day 11 - Factorial of number

---
+++ factorial.py
@@ -0,0 +1,6 @@
+n=int(input("enter a number : "))
+f=1                     #i=f=1
+while n>1:             #while i<=n
+    f=f*n               #f=f*i
+    n=n-1               #i=i+1
+print("factorial =",f)
