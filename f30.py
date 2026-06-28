de09bd32b4fa791ea0c83da35f72734de26624fb
Muskan <muskan22110@gmail.com>
2026-06-28 21:20:30 +0530

    fibo series optimized.

--- f30.py
+++ f30.py
@@ -1,15 +1,8 @@
 # Create a function that returns the nth Fibonacci number.
 def fib(n):
-    a,b=0,1 
-    if n==1:
-        print(a)
-    else:
-        print(a)
-        print(b)
-
-        for i in range(2,n):
-            sum=a+b
-            print(sum) 
-            a,b=b,sum                        
+    a,b=0,1
+    for i in range(n):
+        print(a) 
+        a,b=b,a+b                       
 num=int(input("Enter no. for fibonacci series  : ")) 
 fib(num) 
